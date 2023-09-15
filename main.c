3a1bf0cea49d0c2f6c33c1f1e63e4161b886564e
zakarialoulida <144822861+zakarialoulida@users.noreply.github.com>
2023-09-15 07:09:29 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("veuillez entrer le nombre souhaité a avoir son tableau de multiplication:");
+    scanf("%d",&a);
+    printf("le tableau est le suivant ");
+    for(int i=0;i<10;i++){
+        printf("pour %d *%d = %d \n",i,a,i*a);
+    }
+    return 0;
+}
