2ceaf4f313fffdd39b69856617886da5f9384639
zakarialoulida <144822861+zakarialoulida@users.noreply.github.com>
2023-09-15 09:42:46 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ligne,i,j;
+    scanf("%d",&ligne);
+    for(i=1;i<=(ligne*2);i+=2){
+            for(j=(ligne*2)*2;j>i;j-=2){
+                    printf(" ");
+                    }
+        for(int b=0;b<i;b++){
+            printf("*");
+        }
+    printf("\n");
+    }
+    return 0;
+}
